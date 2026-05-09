7f6295cad5a6dd65f21979726354b42c22573bd5
Avyukth55 <avyukth2511@gmail.com>
2026-05-09 13:24:33 +0800

    Add files via upload
    First Code

---
+++ Printing.py
@@ -0,0 +1,8 @@
+#Printing greeting message
+print ("Hello world")
+#Printing name
+print ("My name is Avyukth")
+#Printing hobby
+print("I love playing footy")
+#Printing math problem
+print ("The result of 5+11 is", 5+11)
